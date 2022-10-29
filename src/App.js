7cb9387176c340bc0773fac7e5668a769e17f4a1
Elem <elem15@yandex.ru>
2022-10-29 22:08:14 +0300

    feat: app work propertly

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('')
+  const [tasks, setTasks] = useState([])
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = new Date().toISOString()
+    const completed = false
+    setTasks([
+      ...tasks, 
+      {id, text, completed},
+    ])
+  }
+  const toggleCheck = (id) => {     
+    setTasks(tasks.map(task => {
+      if (task.id === id) {
+        return {
+          ...task,          
+          completed: !task.completed,
+        }
+      }
+      return task
+    }))
+
+  }
+  const removeTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={text} onChange={(e) => setText(e.target.value)} />
+        <button>add task</button>
+      </form>
+      <ul>
+        {tasks.map(task => <li key={task.id}>
+          <input type="checkbox" className="check" checked={task.completed} onChange={() => toggleCheck(task.id)} />
+          <span>{task.text}</span>
+          <span className='close' onClick={() => removeTask(task.id)}>&#10006;</span>
+        </li>)}
+      </ul>
     </div>
   );
 }
